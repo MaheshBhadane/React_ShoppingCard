1e51b89f2869c24e525aa53b8849999046f30446
MaheshBhadane <mahesh.bhadane@torinit.ca>
2022-07-25 16:05:15 +0530

    week3 task1

--- src/component/NewForm/MainForm.js
+++ src/component/NewForm/MainForm.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 
-
 import "./MainForm.css";
 
 const MainForm = () => {
-    const [enteredImage, setEnteredImage]= useState('');
+  const [enteredImage, setEnteredImage] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredDesc, setEnteredDesc] = useState("");
+  const [enteredPrice, setEnteredPrice] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-    const [enteredTitle, setEnteredTitle] = useState('');
+
+  const imageChangehandler = (event) => {
+    setEnteredImage(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+      return;
+    }
   
-    const [enteredDesc, setEnteredDesc]= useState('');
-
-    const [enteredPrice, setEnteredPrice]= useState('');
-
-    const imageChangehandler= (event) => {
-        setEnteredImage(event.target.files[0]);
-
-        /*if(event.target.name==="img"){
+    /*if(event.target.name==="img"){
             var fread= new FileReader()
             fread.readAsDataURL(event.target.files[0]);
       fread.onloadend = function(event){
@@ -23,77 +25,82 @@ const MainForm = () => {
       }
     }
           */
+  };
 
-      };
-
-    const titleChangeHandler = (event) => {
+  const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+  
   };
 
-  const descChangeHandler= (event) =>{
+  const descChangeHandler = (event) => {
     setEnteredDesc(event.target.value);
+  
   };
 
-  const priceChangeHandler= (event) => {
+  const priceChangeHandler = (event) => {
     setEnteredPrice(event.target.value);
+   
   };
 
-  const titleClearHandler= (event) => {
+  const titleClearHandler = (event) => {
     setEnteredTitle("");
   };
 
-  const submitHandler = (event) =>{
-
+  const submitHandler = (event) => {
     event.preventDefault();
 
-    const itemData={
+    const itemData = {
       image: enteredImage,
-
       title: enteredTitle,
-
       desc: enteredDesc,
-
       price: enteredPrice,
- 
     };
-    console.log(itemData);
+    if (enteredImage.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+      console.log(itemData);
   };
+
+
 
 
   return (
     <>
-    <form onSubmit={submitHandler}>
-      <div className="new-form__controls">
-        <div className="new-form__control">
-          <label> Image </label>
-          <input type="file" onChange={imageChangehandler}/>
-        </div>
-        <div className="new-form__control">
-          <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
-        </div>
-        <div className="new-form__control">
-          <label>Description</label>
-          <input type="text" onChange={descChangeHandler}/>
-        </div>
-      
-        <div className="new-form__control">
-          <label>Amount</label>
-          <input type="number" onChange={priceChangeHandler} />
-        </div>
-        
-      <div className="new-form__control">
-        <button type="submit">Add Item</button>
-      </div>
-        <div className="new-form__control">
-          <button onClick={titleClearHandler}>Clear</button>
-          </div>
+      <form onSubmit={submitHandler}>
+        <div className="new-form__controls">
+
+          <div className="new-form__control">
+            <label style={{ color: !isValid ? "red" : "black" }}> Image </label>
+            <input style={{borderColor: !isValid ? 'red' : 'black',
+        background: !isValid ? 'salmon' : 'transparent'}} type="file" onChange={imageChangehandler} />
           </div>
 
-    </form>
-    <img src={enteredImage} width="150" height="150" alt=""/>
-</>
+          <div className="new-form__control">
+            <label style={{ color: !isValid ? "red" : "black" }}>Title</label>
+            <input style={{borderColor: !isValid ? 'red' : 'black',
+        background: !isValid ? 'salmon' : 'transparent'}} type="text" onChange={titleChangeHandler} />
+          </div>
+          <div className="new-form__control">
+            <label style={{ color: !isValid ? "red" : "black" }}>Description</label>
+            <input type="text" onChange={descChangeHandler} />
+          </div>
 
+          <div className="new-form__control">
+            <label style={{ color: !isValid ? "red" : "black" }}>Amount</label>
+            <input type="number" onChange={priceChangeHandler} />
+          </div>
+
+          <div className="new-form__control">
+            <button type="submit">Add Item</button>
+          </div>
+          <div className="new-form__control">
+            <button onClick={titleClearHandler}>Clear</button>
+          </div>
+        </div>
+      </form>
+      <img src={enteredImage} width="150" height="150" alt="" />
+    </>
   );
 };
 
